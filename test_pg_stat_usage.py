12d4ab891c5f2466d5ae01f88884c435fd09cbdc
Martin Pihlak <martin.pihlak@gmail.com>
2016-12-12 22:12:28 +0200

    Added tests for total and self_time

--- test_pg_stat_usage.py
+++ test_pg_stat_usage.py
@@ -102,7 +102,7 @@ def collect_stat_usage(con):
 		pgsu.set(ur.object_oid, ur.parent_oid, ur)
 	return pgsu
 
-class FunctionCallTest:
+class FunctionCallTester:
 
 	def __init__(self):
 		self.function_oids = {}
@@ -139,7 +139,7 @@ class FunctionCallTest:
 		oid = run_sql_query(self.con, lookup_oid)[0][0]
 		self.function_oids[function_name] = oid
 
-	def assert_calls(self, results, func_name, func_parent_name, calls):
+	def _perform_assertion(self, results, func_name, func_parent_name, assert_function, expected_value):
 		func_id = self.function_oids[func_name]
 		if func_parent_name:
 			parent_id = self.function_oids[func_parent_name]
@@ -151,36 +151,116 @@ class FunctionCallTest:
 		status_msg = ""
 
 		if not usage_data:
-			failed = True
-			status_msg = "no usage data!"
-		elif usage_data.num_calls != calls:
-			failed = True
-			status_msg = "calls=%d" % (usage_data.num_calls)
+			failure, msg = True, "no usage data!"
+		else:
+			failure, msg = assert_function(usage_data, expected_value)
 
-		print '%s assert_calls("%s -> %s", expected_calls=%d): %s' % \
-			(failed and "FAIL" or "PASS", func_parent_name, func_name, calls, status_msg)
+		print '%s %s -> %s %s' % (failure and "FAIL" or "PASS", func_parent_name, func_name, msg)
 
-		if failed:
+		if failure:
+			print "Usage stats dump"
+			print "----------------"
 			for r in results.all():
 				print r
 
-	def execute_function(self, funcname):
-		"""Run the function and collect usage data"""
+	def assert_total_time(self, results, func_name, func_parent_name, duration_ms):
+		"""Validate if total_time meets our expectations"""
+
+		def _fn_assert_total_time(usage_data, expect_total_time_ms):
+			tolerance_ms = 10
+			total_time_ms = int(usage_data.total_time / 1000)
+			if abs(total_time_ms - expect_total_time_ms) > tolerance_ms:
+				return True, "assert_total_time() %d != %d" % \
+							 (total_time_ms, expect_total_time_ms)
+			return False, "assert_total_time() %d ~ %d" % \
+				(total_time_ms, expect_total_time_ms)
+
+		self._perform_assertion(results, func_name, func_parent_name,
+								_fn_assert_total_time, duration_ms)
+
+	def assert_self_time(self, results, func_name, func_parent_name, duration_ms):
+		"""Validate if self_time meets our expectations
+		"""
+		def _fn_assert_self_time(usage_data, expect_self_time_ms):
+			tolerance_ms = 10
+			self_time_ms = int(usage_data.self_time / 1000)
+			if abs(self_time_ms - expect_self_time_ms) > tolerance_ms:
+				return True, "assert_self_time() %d != %d" % \
+							 (self_time_ms, expect_self_time_ms)
+			return False, "assert_self_time() %d ~ %d" % \
+				(self_time_ms, expect_self_time_ms)
+
+		self._perform_assertion(results, func_name, func_parent_name,
+								_fn_assert_self_time, duration_ms)
+
+	def assert_num_calls(self, results, func_name, func_parent_name, expect_num_calls):
+		"""Validate if number of calls matches expected"""
+		def _fn_assert_num_calls(usage_data, expect_num_calls):
+			if usage_data.num_calls != expect_num_calls:
+				return True, "assert_num_calls() %d != %d" % (usage_data.num_calls, expect_num_calls)
+			return False, "assert_num_calls() %d == %d" % (usage_data.num_calls, expect_num_calls)
+
+		self._perform_assertion(results, func_name, func_parent_name,
+								_fn_assert_num_calls, expect_num_calls)
+
+	def execute_functions(self, funcnames):
+		"""Run the functions and collect usage data"""
 		run_sql_query(self.con, "SELECT * FROM pg_stat_usage_reset()")
-		run_sql_query(self.con, "SELECT * FROM %s()" % funcname, do_commit=False)
+		for f in funcnames:
+			run_sql_query(self.con, "SELECT * FROM %s()" % f, do_commit=False)
 		return collect_stat_usage(self.con)
 
+	def execute_function(self, funcname):
+		"""Run a single function and collect usage data"""
+		return self.execute_functions([funcname])
+
 	def test_01_simple_function(self):
-		self.create_simple_function("ff1", "NULL;")
+		self.create_simple_function("ff1", "PERFORM pg_sleep(0.010);")
 		r = self.execute_function("ff1")
-		self.assert_calls(r, "ff1", None, 1)
+
+		self.assert_num_calls(r, "ff1", None, 1)
+		self.assert_total_time(r, "ff1", None, 10)
+		self.assert_self_time(r, "ff1", None, 10)
 
 	def test_02_nested_functions(self):
 		self.create_simple_function("ff2", "PERFORM ff1();")
 		r = self.execute_function("ff2")
-		self.assert_calls(r, "ff2", None, 1)
-		self.assert_calls(r, "ff1", "ff2", 1)
+
+		self.assert_num_calls(r, "ff2", None, 1)
+		self.assert_total_time(r, "ff2", None, 10)
+		self.assert_self_time(r, "ff2", None, 1)
+
+		self.assert_num_calls(r, "ff1", "ff2", 1)
+		self.assert_total_time(r, "ff1", "ff2", 10)
+		self.assert_self_time(r, "ff1", "ff2", 10)
+
+	def test_03_nested_functions(self):
+		r = self.execute_functions(["ff1", "ff2" ])
+		self.assert_num_calls(r, "ff1", None, 1)
+		self.assert_total_time(r, "ff1", None, 10)
+		self.assert_self_time(r, "ff1", None, 10)
+
+		self.assert_num_calls(r, "ff2", None, 1)
+		self.assert_total_time(r, "ff2", None, 10)
+		self.assert_self_time(r, "ff2", None, 1)
+
+		self.assert_num_calls(r, "ff1", "ff2", 1)
+		self.assert_total_time(r, "ff1", "ff2", 10)
+		self.assert_self_time(r, "ff1", "ff2", 10)
+
+	def test_04_call_count_is_cumulative(self):
+		"""Validate that the counts are cumulative"""
+		r = self.execute_functions(["ff2", "ff2" ])
+
+		self.assert_num_calls(r, "ff2", None, 2)
+		self.assert_total_time(r, "ff2", None, 20)
+		self.assert_self_time(r, "ff2", None, 1)
+
+		self.assert_num_calls(r, "ff1", "ff2", 2)
+		self.assert_total_time(r, "ff1", "ff2", 20)
+		self.assert_self_time(r, "ff1", "ff2", 20)
 
 if __name__ == "__main__":
-	fctest = FunctionCallTest()
+	fctest = FunctionCallTester()
 	fctest.run_all_tests()
+
